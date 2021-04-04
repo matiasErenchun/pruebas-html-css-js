d1a1c096db180a032a2f0d564a77d6755b17f752
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2021-04-03 21:29:19 -0400

    primeros passo en angular
    desarrollamos un acumulador

--- CursoAngular/Proyecto1/bases/src/app/app.component.ts
+++ CursoAngular/Proyecto1/bases/src/app/app.component.ts
@@ -1,10 +1,18 @@
+/* tslint:disable:no-inferrable-types */
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
+  /*template: '<span>matias</span>'*/
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string  = 'Contador app';
+  public numero: number = 10;
+  public base: number = 5;
+  /*tslint:disable-next-line:typedef*/
+  public sumar(valor: number)
+  {
+    this.numero += valor;
+  }
 }
